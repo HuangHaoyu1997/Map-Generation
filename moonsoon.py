7ebd42a0fc05a24b1c48eb04e93c7e4518fdf9de
Haoyu Huang <31029753+HuangHaoyu1997@users.noreply.github.com>
2021-09-02 18:41:16 +0800

    update

--- moonsoon.py
+++ moonsoon.py
@@ -79,4 +79,85 @@ plt.colorbar()
 plt.contour(gammas,[0],colors='black')
 plt.show()
 '''
-print(gammas)
+
+# 使用另一层柏林噪声生成山脉。通过使用单独的噪音层，可以更好地控制大陆上有多少座山
+# 为了使地形更有趣，还可以通过另一个柏林噪声扰动坐标以模拟地质构造运动
+# 因此可以生成一些弯曲的山脊
+
+# 生成海拔信息，get elevation
+elevation = np.zeros((size,size))
+for i in range(size):
+    for j in range(size):
+        x,y = i/zoom+x0,j/zoom+y0
+        dx = distortion * noise.pnoise2(x,y,octaves=4,persistence=0.5,lacunarity=2.0,base=seed+15)
+        dy = distortion * noise.pnoise2(x,y,octaves=4,persistence=0.5,lacunarity=2.0,base=seed+23)
+        value = noise.pnoise2(x+dx,y+dy,octaves=6,persistence=0.5,lacunarity=2.0,base=seed+0)*2
+        value = (value + gammas[i,j])/2
+        elevation[i,j] = value
+'''
+plt.figure(figsize=(10,10))
+plt.imshow(gammas,cmap='Blues')
+plt.colorbar()
+plt.imshow(np.ma.masked_where(elevation<0,elevation),cmap='summer') # 显示海拔低于0的区域
+plt.colorbar()
+plt.contour(np.clip(elevation,0,1),colors='black',alpha=0.1)
+plt.contour(elevation,[0.5],colors='black',alpha=0.5)
+plt.contour(elevation,[0],colors='black')
+plt.title("Perlin Noise")
+plt.show()
+'''
+
+# 为了使大陆更加连续，通过泛洪算法[flood fill](https://en.wikipedia.org/wiki/Flood_fill) 
+# 将小的内陆海洋的海拔调整到略高于海平面的高度，这样就得到一些像样的平原
+
+@njit
+def fill_ocean(elevation,minsize,threshold=0,target=0.05):
+    '''
+    elevation：海拔地图
+    minsize：小于该面积的ocean将被填充
+    threshold：小于该值的区域将被替换成target
+    target：目标值
+    '''
+    visited = elevation < -1 # 海拔低于-1的区域不考虑替换
+    dirs = ((0,1),(1,0),(0,-1),(-1,0))
+    for i0 in range(size):
+        for j0 in range(size):
+            if elevation[i0,j0] <= threshold and not visited[i0,j0]:
+                opened = []
+                closed = []
+                count = 0
+                opened.append((i0,j0))
+                visited[i0,j0] = True
+                count += 1
+                while len(opened)>0:
+                    i,j = opened.pop()
+                    closed.append((i,j))
+                    for oo,delta in enumerate(dirs):
+                        ii,jj = i+delta[0],j+delta[1]
+                        if 0<=ii and ii<size and 0<=jj and jj<size: # 得到当前点的上下左右四个邻点，且判断不超过地图范围
+                            if not visited[ii,jj] and elevation[ii,jj]<=threshold:
+                                opened.append((ii,jj))
+                                visited[ii,jj]=True
+                                count+=1
+                        else:
+                            count += 2*zoom
+                if count<minsize: # 总面积小于minsize，则认定为"small ocean"，需要被填充
+                    for p in closed:
+                        elevation[p[0],p[1]]=target
+    return elevation
+# 移除面积过小的内湖
+elevation = fill_ocean(elevation,2*zoom**2,-0.1,0.1)
+elevation = fill_ocean(elevation,2*zoom**2,0.01,0.05)
+# 移除小岛，替换为海域
+elevation = -fill_ocean(-elevation,5,0,0.05)
+'''
+plt.figure(figsize=(12,12))
+plt.imshow(gammas,cmap='Blues')
+plt.imshow(np.ma.masked_where(elevation<0,elevation),cmap='summer')
+plt.contour(np.clip(elevation,0,1),colors='black',alpha=0.1)
+plt.contour(elevation,[0.5],colors='black',alpha=0.5)
+plt.contour(elevation,[0],colors='black')
+plt.title('Landmass')
+plt.show()
+'''
+
